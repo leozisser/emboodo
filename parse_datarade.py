d5e27c10408e9155da1ab7365bdeda1507197159
leozisser <leozisser@gmail.com>
2022-01-30 11:47:13 +0200

    postprocessing for the task of Jan 27; restructured parse_datarade

--- parse_datarade.py
+++ parse_datarade.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
 import numpy as np
 from bs4 import BeautifulSoup
 import os
@@ -18,7 +21,10 @@ alternatives = '/alternatives'
 classname_alternative = 'provider-profile__alternative__header-name'
 classname_datacat = "tag-label tag-label--green"
 classname_usecase = "tag-label tag-label--blue"
-
+xpath_search_bar = '/html/body/div[1]/header/div[1]/div/div/form/div/div[1]/input[2]'
+xpath_1st_result = '/html/body/div[1]/header/div[1]/div/div/form/div/div[2]/a[1]/div/div[2]/div[1]'
+# /html/body/div[1]/header/div[1]/div/div/form/div/div[2]/a[1]/div/div[2]/div[1]
+# /html/body/div[1]/header/div[1]/div/div/form/div/div[2]/a/div/div/div/text()
 xlpath = '/Users/leo_z/Downloads/Data-Hunters Data Providers Connections.xlsx'
 data_providers = pd.read_excel(xlpath,sheet_name=0).fillna('').dropna()
 data_categories = pd.read_excel(xlpath,sheet_name=1).fillna('')
@@ -32,7 +38,7 @@ chrome_options.add_argument('--disable-logging')
 chrome_options.add_argument('--incognito')
 chrome_options.add_argument('--disable-infobars')
 driver = webdriver.Chrome('/Users/leo_z/Documents/GitHub/emboodo/chromedriver')
-# driver2 = webdriver.Chrome('/Users/leo_z/Documents/GitHub/emboodo/chromedriver',chrome_options=chrome_options)
+driver2 = webdriver.Chrome('/Users/leo_z/Documents/GitHub/emboodo/chromedriver',chrome_options=chrome_options)
 
 # unknown_providers = pd.read_excel(xlpath,sheet_name=5).fillna('')
 # uk_pr = pd.merge(unknown_providers, data_providers, on = ['post_title']).drop_duplicates()
@@ -96,9 +102,9 @@ def get_related_data_providers(driver, provider):
 def get_related_datasets(soup):
     classname_datasets = 'vertical-product-card__title'
     related_list_html = soup.findAll(attrs={"class" : classname_datasets}) 
-    related_list_raw = [i.text for i in related_list_html]
-    related_list = pick_existing_entries(related_list_raw,datasets)
-    return related_list
+    related_list_raw = [i.text.replace('\n','') for i in related_list_html]
+    # related_list = pick_existing_entries(related_list_raw,datasets)
+    return related_list_raw
 
 def table_row_providers(row):
     name = row['post_title']
@@ -139,24 +145,31 @@ def table_row_datasets(row,driver):
     print(name)
     # descr = row['post_content'].replace('\n','').replace('\r','')
     # descr2 = row['post_excerpt'].replace('\n','').replace('\r','')
-    driver.find_element("xpath",'/html/body/div[1]/header/div[1]/div/div/form/div/div[1]/input[2]').send_keys(name)#.send_keys(Keys.RETURN)
-    time.sleep(0.5)
+    driver.find_element("xpath",xpath_search_bar).send_keys(name)#.send_keys(Keys.RETURN)
+    # time.sleep(2)
+    timeout = 2
     try:
-        driver.find_element("xpath",'/html/body/div[1]/header/div[1]/div/div/form/div/div[2]/a[1]').click()
-        time.sleep(1)
-        s = driver.page_source
+        element_present = EC.presence_of_element_located((By.XPATH, xpath_1st_result))
+        WebDriverWait(driver, timeout).until(element_present)
         text_of_ban = driver.find_element("xpath","/html/body")
         if text_of_ban.text =="Too many requests in a given amount of time. We employ rate limiting to ensure the stability of our services to all users. If you think this is a mistake, contact us at platform@datarade.ai":
             raise ValueError('banned, switch to a different VPN')
+        driver.find_element("xpath",xpath_1st_result).click()
+        time.sleep(1)
+        s = driver.page_source
         soup = BeautifulSoup(s, features="html.parser")
         data_cat_list = get_data_cat_list(soup)
         usecase_list = get_usecase_list(soup)
         related_list = get_related_datasets(soup)
+        print('DATA', data_cat_list, usecase_list)
+        print('RELATED', related_list)
         row['related_data_sets'] = ','.join(related_list)
         row['use_case'] = ','.join(checked_for_existence(usecase_list,cases))
         row['data_category'] = ','.join(checked_for_existence(data_cat_list, categories))
-    except:
+    except Exception as err:
+        # lol()
         print('FUCK NO', name)
+        print(err)
         row['related_data_sets'] = 'none found'
         row['use_case'] = 'none found'
         row['data_category'] = 'none found'
@@ -164,6 +177,8 @@ def table_row_datasets(row,driver):
 
 
 def handle_table(df, batch_size, start,mode):
+    driver = webdriver.Chrome('/Users/leo_z/Documents/GitHub/emboodo/chromedriver')
+    driver2 = webdriver.Chrome('/Users/leo_z/Documents/GitHub/emboodo/chromedriver',chrome_options=chrome_options)
     # print(df)
     cnt = 0
     df = df.dropna()
@@ -191,14 +206,15 @@ def scrape_sheet(sheet, mode='datasets'):
             tx = text_file.read()
             n0 = int(tx)
     except:
-        n0=-1
+        n0=0
     while n0<len(sheet):
-        n0+=batch_size   
+           
         print(n0)
         df_new = handle_table(sheet,batch_size,n0,mode=mode)
+        n0+=batch_size
         hdr = (n0 == 0)      
         df_new.to_csv('newdf.csv',mode='a',header=hdr,index = False)
         with open("current.txt", "w") as text_file:
             text_file.write(str(n0))
 
-scrape_sheet(datasets_df[:1])
+# scrape_sheet(datasets_df)
